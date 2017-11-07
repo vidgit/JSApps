8d7acb33db0e0101a7798d81f4322aa3e4ab13dd
Vibhor Joshi <vibhorjoshi05@gmail.com>
2017-11-07 14:57:43 +0530

    Syntax Cheatsheet added.

---
+++ Cheatsheet.js
@@ -0,0 +1,14 @@
+//Object Syntax
+var obj={
+  property1:"1",
+  property2:"2",
+  property3:"3",
+  function1: function(){}
+};
+
+//Constructor Syntax
+function Function(p1,p2,p3){
+  this.property1=p1;
+  this.property2=p2;
+  this.property3=p3;
+}
