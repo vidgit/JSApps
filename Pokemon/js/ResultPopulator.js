8df07743cc458063dc8963bd8a6447f98ec5d6d6
Vibhor Joshi <vibhorjoshi05@gmail.com>
2017-11-15 12:57:56 +0530

    Results Page

--- Pokemon/js/ResultPopulator.js
+++ Pokemon/js/ResultPopulator.js
@@ -4,7 +4,7 @@ var pokemon={
     "id":"001",
     "type1":"Grass",
     "type2":"Poison",
-    "img":"img/001.jpg",
+    "img":"img/001.png",
     "about":"Bulbasaur (フシギダネ Fushigidane) is a Grass/Poison-type Pokémon introduced in Generation I. It evolves into Ivysaur starting at level 16. It is one of the three Starter Pokémon that can be chosen in the Kanto region.",
   },
   "2":{
