740dfffe2c5e1d0617bbc057f9075e533f4bf42b
Vibhor Joshi <vibhorjoshi05@gmail.com>
2017-11-17 12:26:33 +0530

    Text Hiding Done

---
+++ PokemonNameGame/js/main.js
@@ -0,0 +1,10 @@
+var pokemonList={"bulbasaur":{"hidden":true},
+                  "ivysaur":{"hidden":true},
+                  "venusaur":{"hidden":true},
+                  "charmander":{"hidden":true},
+                  "charmeleon":{"hidden":true},
+                  "charizard":{"hidden":true},
+                  "squirtle":{"hidden":true},
+                  "wartortle":{"hidden":true},
+                  "blastoise":{"hidden":true}
+                };
