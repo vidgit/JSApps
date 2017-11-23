efd1392090f76b244217307aa762f8c90e747b04
Vibhor Joshi <vibhorjoshi05@gmail.com>
2017-11-23 22:09:54 +0530

    Relearning JS

--- Cheatsheets/Cheatsheet.js
+++ Cheatsheets/Cheatsheet.js
@@ -13,8 +13,21 @@ function Function(p1,p2,p3){
   this.property3=p3;
 }
 //HTTP request Syntax
+function httprequest(){
 var xhr= new XMLHttpRequest();
 xhr.open("GET","https://www.somesite.com",false);
 xhr.send();
+}
 
 console.log(xhr.status);
+
+//Basic JS
+function basicJS(){
+  var anInt="12";
+  var aFloat="12.5";
+
+  console.log(anInt+aFloat);
+  console.log(parseInt(anInt)+parseInt(aFloat));
+  console.log(parseFloat(anInt)+parseFloat(aFloat));
+  console.log(parseInt(anInt)+parseFloat(aFloat));
+}
