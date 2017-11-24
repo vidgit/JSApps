2864358e718087eb820837da2692c39dcebb8748
Vibhor Joshi <vibhorjoshi05@gmail.com>
2017-11-24 09:11:30 +0530

    More notes

--- Cheatsheets/Cheatsheet.js
+++ Cheatsheets/Cheatsheet.js
@@ -32,3 +32,21 @@ function basicJS(){
   console.log(parseFloat(anInt)+parseFloat(aFloat));
   console.log(parseInt(anInt)+parseFloat(aFloat));
 }
+
+function varTest() {
+ var x = 1;
+ if (true) {
+   var x = 2;  // same variable!
+   console.log(x);  // 2
+ }
+ console.log(x);  // 2
+}
+
+function letTest() {
+ let x = 1;
+ if (true) {
+   let x = 2;  // different variable
+   console.log(x);  // 2
+ }
+ console.log(x);  // 1
+}
