a17cdae8cd78f0e1662f14a418e0437fa2076e02
Vibhor Joshi <vibhjosh@qti.qualcomm.com>
2017-12-21 20:53:21 +0530

    Functions Study

--- Cheatsheets/Cheatsheet.js
+++ Cheatsheets/Cheatsheet.js
@@ -50,3 +50,18 @@ function letTest() {
  }
  console.log(x);  // 1
 }
+
+var kebabToSnake= function(){
+  var mystring=document.getElementById('kts').value;
+  console.log(mystring.replace(/-/g,"_"));
+};
+
+
+var x = setInterval(function(){
+  console.log("No Button used.");
+},2000);
+
+var touched = function(){
+  console.log("Touched")
+  clearInterval(x);
+};
