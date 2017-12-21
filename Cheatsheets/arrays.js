644a01499b7873770a7be3bbb4d3eb6de0677b4a
Vibhor Joshi <vibhorjoshi05@gmail.com>
2017-12-21 22:17:07 +0530

    arrays

--- Cheatsheets/arrays.js
+++ Cheatsheets/arrays.js
@@ -1,8 +1,15 @@
 var arr=[1,"1",true,NaN];
+arr[10]=0;
+console.log(arr);
+for (var i in arr) {
+  console.log(i+"\n");
+}
+function truthValue(){
 for(var i=0;i<arr.length;i++){
   if(arr[i])
     console.log(true);
   else {
     console.log(false);
   }
+}
 }
