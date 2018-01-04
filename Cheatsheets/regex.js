34081c5b50e86b2a8793bacc1f7755babd63101e
Vibhor Joshi <vibhorjoshi05@gmail.com>
2018-01-04 23:51:42 +0530

    Layouts

--- Cheatsheets/regex.js
+++ Cheatsheets/regex.js
@@ -1,6 +1,18 @@
-var re=/^[aeiou](.*\1)$/;
-var words=["aeiou","aeioua"];
-for(word in words){
-  console.log(re.test(word));
-  console.log(word.match(re));
+var re=/^([aeiou])(.*\1)$/;
+var words=["aeiou","aeioua","Mr.A","Dr.X","Mrs.l","kr.a","pms.l","Dr.Er.","1","123.4",'102', '1948948', '1', '3', '4', '5'];
+console.log("Vowel Test");
+for(word of words){
+  console.log(word,re.test(word),word.match(re));
+}
+
+re=/^((Mr\.)|(Dr\.)|(Mrs\.)|(Ms\.)|(Er\.))[a-zA-z]+$/g;
+console.log("Mr etc Test");
+for(word of words){
+  console.log(word,re.test(word),word.match(re));
+}
+
+re=/\d+/g;
+console.log("Numbers");
+for(word of words){
+  console.log(word,re.test(word),word.match(re));
 }
